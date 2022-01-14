0ce7b510fcd852541a99052a6820828fb6d21300
MilanGovedaricaRA15 <73367567+MilanGovedaricaRA15@users.noreply.github.com>
2022-01-14 02:23:01 +0100

    #4.7 TravisCl + 3 unit testa

--- IzdajMe/src/test/java/com/izdajMe/izdajMe/IzdajMeApplicationTests.java
+++ IzdajMe/src/test/java/com/izdajMe/izdajMe/IzdajMeApplicationTests.java
@@ -1,13 +1,18 @@
 package com.izdajMe.izdajMe;
 
+import com.izdajMe.izdajMe.model.Cottage;
+import com.izdajMe.izdajMe.model.User;
 import org.junit.jupiter.api.Test;
+import org.junit.runner.RunWith;
 import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.test.context.junit4.SpringRunner;
 
-@SpringBootTest
-class IzdajMeApplicationTests {
+@RunWith(SpringRunner.class)
+@SpringBootTest(classes = {Cottage.class, User.class})
+public class IzdajMeApplicationTests {
 
 	@Test
-	void contextLoads() {
+	public void contextLoads() {
 	}
 
 }
