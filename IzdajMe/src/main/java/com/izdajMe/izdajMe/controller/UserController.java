d88f3c2a4e1fa2a157cc06daa87ef3e8cabfdb1a
MilanGovedaricaRA15 <73367567+MilanGovedaricaRA15@users.noreply.github.com>
2022-01-21 12:44:15 +0100

    jos ispravljanja bagova pri deploy

--- IzdajMe/src/main/java/com/izdajMe/izdajMe/controller/UserController.java
+++ IzdajMe/src/main/java/com/izdajMe/izdajMe/controller/UserController.java
@@ -13,7 +13,7 @@ import java.util.ArrayList;
 import java.util.List;
 
 @RestController
-@CrossOrigin(origins = "https://izdajme.herokuapp.com")
+@CrossOrigin(origins = "https://izdajme.herokuapp.com/")
 
 public class UserController {
 
