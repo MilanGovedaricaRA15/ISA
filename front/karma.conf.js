ff7a3c1c9a9d12cfd008ce5f3131d58c8be0518e
MilanGovedaricaRA15 <73367567+MilanGovedaricaRA15@users.noreply.github.com>
2022-01-21 02:41:14 +0100

    ispravljen bag s promjenom imena projekta na frontu

--- front/karma.conf.js
+++ front/karma.conf.js
@@ -25,7 +25,7 @@ module.exports = function (config) {
       suppressAll: true // removes the duplicated traces
     },
     coverageReporter: {
-      dir: require('path').join(__dirname, './coverage/IzdajMe'),
+      dir: require('path').join(__dirname, './coverage/izdaj-me'),
       subdir: '.',
       reporters: [
         { type: 'html' },
